da508722dd00e92ea62ce5b5e5a446a1df632a4b
Jafed Hernandez <jafed56david@gmail.com>
2024-10-27 18:15:26 -0500

    update UI

--- README.md
+++ README.md
@@ -2,13 +2,27 @@
 
 ## Package info
 
+This package provides the `AppPokemon` component, a custom LitElement-based element that allows viewing and searching information about Pokémon using the PokéAPI.
+
 ### Package installation
 
 Installation using NPM
 
 ```bash
-npm install @meraki/app-pokemon
+npm install @meraki/app-pokemon 
 ```
+packege.json:  "dependencies": {
+    "@meraki/pokemon-dm": "version"
+  },
+ 
+```bash
+npm install 
+``` 
+### Prerequisites
+
+- Node.js v14+
+- LitElement v2+
+- Access to the PokéAPI for real-time data consumption
 
 ### Entry points & exports
 
@@ -52,15 +66,76 @@ import '@meraki/app-pokemon/app-pokemon.js';
 
 ![LitElement component](https://img.shields.io/badge/litElement-component-blue.svg)
 
-This component ...
+This component, The AppPokemon component is a custom web component built with LitElement for displaying and searching Pokémon. This component allows users to view Pokémon details, search by name or ID, and see evolution chains where available. It fetches data from the PokéAPI and provides features such as pagination, evolution views, and error handling through a modal.
 
 Example:
 
 ```html
   <app-pokemon></app-pokemon>
+  <app-pokemon
+  searchQuery="bulbasaur"
+  perPage="10"
+  currentPage="1"
+  detailOpened="true"
+></app-pokemon>
+
 ```
+![app-pokemon](resource/pokemon-ui.png)
+
 
 ### Properties
 
-- **name**: string = "Cells" (attribute: name)
-    Description for property
+- **pokemons**: `Array`  
+  _(type: Array)_  
+  Description: List of all available Pokémon.
+
+- **searchResults**: `Array`  
+  _(type: Array)_  
+  Description: List of Pokémon search results.
+
+- **selectedEvolutions**: `Array`  
+  _(type: Array)_  
+  Description: Selected evolutions for the current Pokémon.
+
+- **searchQuery**: `string`  
+  _(type: String)_  
+  Description: Current search query to find Pokémon.
+
+- **currentPage**: `number`  
+  _(type: Number)_  
+  Description: Current page number in the list of results.
+
+- **totalPokemons**: `number`  
+  _(type: Number)_  
+  Description: Total number of Pokémon available in the database.
+
+- **perPage**: `number`  
+  _(type: Number)_  
+  Description: Number of Pokémon displayed per page.
+
+- **loading**: `boolean`  
+  _(type: Boolean)_  
+  Description: Loading state of the application (true if loading).
+
+- **detailOpened**: `boolean`  
+  _(type: Boolean, attribute: false)_  
+  Description: Indicates if the detail view is open or closed.
+
+### BBVA Usage Component
+
+The BBVA usage components are designed to facilitate seamless interactions within web applications. Each component serves a specific purpose, enhancing user experience and functionality.
+
+- **bbva-web-button-default**: A customizable button component that provides users with a consistent and visually appealing interface for performing actions throughout the application. It supports various states (default, hover, active) and can be easily styled to match the application's branding.
+![bbva-web-button-default](resource/button.png)
+
+
+- **bbva-progress-content**: This component displays progress indicators for ongoing tasks, allowing users to visualize the completion status of processes such as uploads, downloads, or any time-consuming actions. It improves user engagement by providing feedback during long-running operations.
+![bbva-progress-content](resource/progress.png)
+
+
+- **bbva-help-modal**: A modal dialog that offers contextual help and guidance to users. This component can display FAQs, tips, or detailed instructions to assist users in navigating the application effectively. It enhances usability by ensuring that help is readily available when needed.
+![bbva-help-modal](resource/modal.png)
+
+
+
+This README includes more complete HTML examples, demonstrating how to integrate the `app-pokemon` component into an HTML application with configured events and properties.
--- src/AppPokemon.js
+++ src/AppPokemon.js
@@ -1,347 +1,212 @@
-import { LitElement, html, css } from 'lit-element';
+import { LitElement, html } from 'lit-element';
 import styles from './app-pokemon.css.js';
 import '@bbva-web-components/bbva-web-button-default/bbva-web-button-default.js';
 import '@bbva-web-components/bbva-progress-content/bbva-progress-content.js';
 import '@bbva-web-components/bbva-help-modal/bbva-help-modal.js'
+import '@meraki/pokemon-dm/pokemon-dm.js'
 
 
 export class AppPokemon extends LitElement {
-  
-  static get properties() {
-    return {
-      pokemons: { type: Array },
-      currentPage: { type: Number },
-      searchQuery: { type: String },
-      totalPokemons: { type: Number },
-      perPage: { type: Number },
-      searchResults: { type: Array },
-      selectedEvolutions: { type: Array },
-      loading: {type: Boolean},
-      detailOpened: {type: Boolean, attribute: false,},
-    };
-  }
 
-  constructor() {
-    super();
-    this.pokemons = [];
-    this.currentPage = 1;
-    this.searchQuery = '';
-    this.totalPokemons = 0;
-    this.perPage = 10;
-    this.selectedEvolutions = null;
-    this.searchResults = [];
-    this.isModalOpen = false;
-    this.modalMessage = '';
-  }
-
-  static get styles() {
-    return [
-      styles,
-    ]; 
-  }
-
-  async fetchPokemons() {
-    const offset = (this.currentPage - 1) * this.perPage;
-
-    try {
-      const response = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=${this.perPage}&offset=${offset}`);
-
-      if (!response.ok) {
-        throw new Error(`Error en la solicitud: ${response.status}`);
-      }
-      const data = await response.json();
-
-      const detailedPokemons = await Promise.all(data.results.map(async(pokemon) => {
-        const pokemonResponse = await fetch(pokemon.url);
-        if (!pokemonResponse.ok) {
-          throw new Error(`Error al obtener datos de Pokémon: ${pokemonResponse.status}`);
-        }
-        return await pokemonResponse.json();
-      }));
-
-      this.pokemons = detailedPokemons;
-      this.pokemons.sort((a, b) => a.weight - b.weight);
-      this.totalPokemons = data.count;
-
-      this.dispatchEvent(new CustomEvent('my-data-request-success', {
-        detail: { pokemons: this.pokemons, total: this.totalPokemons },
-        bubbles: true,
-        composed: true
-      }));
-
-    } catch (error) {
-      console.error('Error en fetchPokemons:', error);
-
-      this.dispatchEvent(new CustomEvent('my-data-request-failure', {
-        detail: error,
-        bubbles: true,
-        composed: true
-      }));
+    static get properties() {
+        return {
+            pokemons: { type: Array },
+            searchResults: { type: Array },
+            selectedEvolutions: { type: Array },
+            searchQuery: { type: String },
+            currentPage: { type: Number },
+            totalPokemons: { type: Number },
+            perPage: { type: Number },
+            loading: { type: Boolean },
+            detailOpened: { type: Boolean, attribute: false, },
+        };
     }
-  }
 
-  willBeActive(active) {
-    if (active) {
-      this.fetchPokemons();
-    } else {
-      this.reset();
-    }
-  }
-
-  async searchPokemons() {
-    if (this.searchQuery.length < 1) {
-      this.pokemons = [];
-      return;
-    }
-  
-    const idQuery = parseInt(this.searchQuery);
-
-    if (!isNaN(idQuery)) {
-      try {
-        const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${idQuery}`);
-        if (response.ok) {
-          this.pokemons = [await response.json()]; 
-        } else {
-          this.pokemons = [];
-        }
-      } catch (error) {
-        console.error('Error al buscar Pokémon por ID:', error);
+    constructor() {
+        super();
         this.pokemons = [];
-      }
-    } else {
-      try {
-        const allPokemonsResponse = await fetch('https://pokeapi.co/api/v2/pokemon?limit=1000');
-        const allPokemons = await allPokemonsResponse.json();
-  
-        this.pokemons = await Promise.all(allPokemons.results
-          .filter(pokemon => pokemon.name.startsWith(this.searchQuery.toLowerCase()))
-          .map(async pokemon => {
-            const pokemonResponse = await fetch(pokemon.url);
-            return pokemonResponse.ok ? await pokemonResponse.json() : null;
-          })
-        );
-      } catch (error) {
-        console.error('Error al buscar Pokémon por nombre:', error);
-        this.pokemons = [];
-      }
+        this.currentPage = 1;
+        this.searchQuery = '';
+        this.totalPokemons = 0;
+        this.perPage = 10;
+        this.selectedEvolutions = [];
+        this.searchResults = [];
+        this.loading = false;
+        this.isModalOpen = false;
+        this.visiblePages = [1,2];
     }
-  }
-  
 
-  handleInput(ev) {
-    this.searchQuery = ev.target.value;
-    this.searchPokemons();
-  }
-
-  async changePage(page) {
-    this.currentPage = page;
-    await this.fetchPokemons();
-  }
-
-  async nextPage() {
-    if (this.currentPage < this.totalPages) {
-      this.currentPage++;
-      await this.fetchPokemons();
+    static get styles() {
+        return [
+            styles,
+        ];
     }
-  }
+   
 
-  async prevPage() {
-    if (this.currentPage > 1) {
-      this.currentPage--;
-      await this.fetchPokemons();
-    }
-  }
-
-  get totalPages() {
-    return Math.ceil(this.totalPokemons / this.perPage);
-  }
-
-  get visiblePages() {
-    const total = this.totalPages;
-    const pages = [];
-    const startPage = Math.max(1, this.currentPage - 1);
-    const endPage = Math.min(total, this.currentPage + 1);
-    for (let i = startPage; i <= endPage; i++) {
-      pages.push(i);
-    }
-    return pages;
-  }
-
-  async fetchEvolutionChain(pokemon) {
-    try {
-      const speciesResponse = await fetch(pokemon.species.url);
-      const speciesData = await speciesResponse.json();
-
-      if (speciesData.evolution_chain) {
-        const evolutionChainResponse = await fetch(speciesData.evolution_chain.url);
-        const evolutionChainData = await evolutionChainResponse.json();
-        return evolutionChainData;
-      }
-      return null;
-    } catch (error) {
-      console.error("Error al obtener la cadena de evolución:", error);
-      return null;
-    }
-  }
-
-  async showEvolution(pokemon) {
-    const evolutionChain = await this.fetchEvolutionChain(pokemon);
-    if (evolutionChain) {
-      const evolutions = await this.getEvolutions(evolutionChain.chain);
-        this.selectedEvolutions = evolutions;
-    } 
-  }
-
-  async getEvolutions(chain) {
-    const evolutions = [];
-    let current = chain;
-  
-    while (current) {
-      const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${current.species.name}`);
-      if (response.ok) {
-        const pokemonData = await response.json();
-        evolutions.push(pokemonData);
-      }
-      current = current.evolves_to[0] || null; 
-    }
-    return evolutions;
-  }
-  
-
-  goBackToList() {
-    this.selectedEvolutions = null;
-  }
-
-  firstUpdated() {
-    this.fetchPokemons();
-  }
-
-  async _submit(pokemon, ev) {
-    ev.preventDefault(); 
-    this.loading = true; 
-
-    setTimeout(async () => {
-        try {
-            const evolutionChain = await this.fetchEvolutionChain(pokemon);
-            if (evolutionChain) {
-                const evolutions = await this.getEvolutions(evolutionChain.chain);
-                
-                
-                if (evolutions.length > 1) {
-                    this.showEvolution(pokemon)
-                } else {
-                    
-                    this.showErrorModal('Some Pokémon have not evolved yet or there is no record of their evolution. We invite you to search for another Pokémon that has an evolution.');
-                }
-            } 
-        } catch (error) {
-            console.error("Error al mostrar la evolución:", error);
-            this.showErrorModal('Ha ocurrido un error inesperado. Por favor, intenta de nuevo.'); 
-        } finally {
-            this.loading = false; 
-        }
-    }, 300); 
+    async firstUpdated(props) {
+        super.firstUpdated && super.firstUpdated(props);
+        const queryScope = this.shadowRoot || this;
+        this._dm = queryScope.querySelector('pokemon-dm');
+        await this._dm.fetchPokemons();
+        this.updateState();
 }
 
-
-  showErrorModal(message) {
-    this.modalMessage = message;
-    this.isModalOpen = true;
-    const modal = this.shadowRoot.getElementById('modal-error');
-    if (modal) {
-      modal.bodyText = this.modalMessage;
-      modal.open();
+    updateState() {
+        this.pokemons = this._dm.pokemons;
+        this.currentPage = this._dm.currentPage;
+        this.totalPages = this._dm.totalPages;
+        this.visiblePages = this._dm.visiblePages;
+        this._dm.searchQuery = this.searchQuery
+        this.selectedEvolutions = this._dm.selectedEvolutions
+        this.requestUpdate();
     }
-  }
 
-  closeModal() {
-    const modal = this.shadowRoot.getElementById('modal-error');
-    if (modal) {
-      modal.close();
+    async changePage(delta) {
+        await this._dm.changePage(delta);
+        this.updateState();
     }
-    this.isModalOpen = false;
-  }
+ 
+    async handleInput(ev) {
+        this.searchQuery = ev.target.value;
+        this.updateState();
+        await this._dm.searchPokemons()
+        this.updateState();
+    }
 
-  render() {
-    return html`
-      <div>
     
-        <h1>Pokémon App</h1>
+    goBackToList() {
+        this._dm.selectedEvolutions = null;
+        this.updateState();
+    }
 
-        <div class="search-bar">
-          <input id="searchInput" type="text" placeholder="Search by ID or Name" @input="${this.handleInput}">
-        </div>
+    async submitPokemon(pokemon, ev) {
+        ev.preventDefault();
+        this.loading = true;
 
-        ${this.loading ? html`
-          <bbva-progress-content visible></bbva-progress-content>
-    ` : html`
+        setTimeout(async () => {
+            try {
+                const evolutionChain = await this._dm.fetchEvolutionChain(pokemon);
+                if (evolutionChain) {
+                    const evolutions = await this._dm.getEvolutions(evolutionChain.chain);
 
-        <div class="pokemon-container">
-          <div class="pokemon-list">
-            ${this.pokemons.map(pokemon => html`
-              <div class="pokemon-card">
-                <h2>${pokemon.name}</h2>
-                <img src="${pokemon.sprites.other.dream_world.front_default}" alt="${pokemon.name}">
-                <p>Height: ${pokemon.height}</p>
-                <p>Weight: ${pokemon.weight}</p>
-                <p>Type: ${pokemon.types.map(typeInfo => typeInfo.type.name).join(', ')}</p>
-        
-                <bbva-web-button-default @click="${(ev) => this._submit(pokemon, ev)}">
-                  Show Evolution
-                </bbva-web-button-default>
-              </div>
-            `)}
-          </div>
-        </div>
-        `}
+                    if (evolutions.length > 1) {
+                        await this._dm.showEvolution(pokemon)
+                        this.updateState();
+                    } else {
+                        this.showErrorModal('Some Pokémon have not evolved yet or there is no record of their evolution. We invite you to search for another Pokémon that has an evolution.');
+                    }
+                }
+            } catch (error) {
+                this.showErrorModal('Ha ocurrido un error inesperado. Por favor, intenta de nuevo.');
+            } finally {
+                this.loading = false;
+            }
+        }, 300);
+    }
 
-       <div class="pagination">
-        <bbva-web-button-default @click="${this.prevPage}" ?disabled="${this.currentPage === 1}">Previous</bbva-web-button-default>
-          ${this.visiblePages.map(page => html`
-        <bbva-web-button-default @click="${() => this.changePage(page)}" class="${page === this.currentPage ? 'active' : ''}">${page}</bbva-web-button-default>
-        `)}
-        <bbva-web-button-default @click="${this.nextPage}" ?disabled="${this.currentPage === this.totalPages}">Next</bbva-web-button-default>
-      </div>
+    showErrorModal(message) {
+        this.modalMessage = message;
+        this.isModalOpen = true;
 
-      <div class="evolution-container ${this.selectedEvolutions ? 'show-evolution' : 'evolution-overlay'}">
-        ${this.selectedEvolutions && this.selectedEvolutions.length > 0 ? html`
-        <div class="evolution-content">
-            <h1>Evolution Pokemon</h1>
-            <div class="pokemon-text">
-                ¡Pokémon evolutions are like watching your little friend go from cute to fearsome! Just a couple of levels, a flash of light, and boom! Now you have a powerful beast that can shoot bolts and crush rivals. It's the magic of levels and stats skyrocketing!
+        const modal = this.shadowRoot.getElementById('modal-error');
+        if (modal) {
+            modal.bodyText = this.modalMessage;
+            modal.open();
+        } else {
+            this.isModalOpen = false;
+        }
+    }
+
+    render() {
+        return html`
+          <div>
+            <h1>Pokémon App</h1>
+    
+            <div class="search-bar">
+              <input id="searchInput" type="text" placeholder="Search by ID or Name" @input="${this.handleInput}">
             </div>
-            <div class="evolution-cards">
-                ${this.selectedEvolutions.map(evolution => html`
-                    <div class="evolution-card">
+    
+            ${this.loading ? html`
+              <bbva-progress-content visible></bbva-progress-content>
+            ` : html`
+              <div class="pokemon-container">
+                <div class="pokemon-list">
+                  ${this.pokemons.map(pokemon => html`
+                    <div class="pokemon-card">
+                      <h2>${pokemon.name}</h2>
+                      <img src="${pokemon.sprites.other.dream_world.front_default}" alt="${pokemon.name}">
+                      <p>Height: ${pokemon.height}</p>
+                      <p>Weight: ${pokemon.weight}</p>
+                      <p>Type: ${pokemon.types.map(typeInfo => typeInfo.type.name).join(', ')}</p>
+                      <bbva-web-button-default @click="${(ev) => this.submitPokemon(pokemon, ev)}">
+                        Show Evolution
+                      </bbva-web-button-default>
+                    </div>
+                  `)}
+                </div>
+              </div>
+            `}
+    
+            <div class="pagination">
+              <bbva-web-button-default 
+                @click="${() => this.changePage(-1)}" 
+                ?disabled="${this.currentPage === 1}">
+                Previous
+              </bbva-web-button-default>
+    
+              ${this.visiblePages.map(page => html`
+                <bbva-web-button-default 
+                  @click="${() => this.changePage(page - this.currentPage)}" 
+                  class="${page === this.currentPage ? 'active' : ''}">
+                  ${page}
+                </bbva-web-button-default>
+              `)}
+    
+              <bbva-web-button-default 
+                @click="${() => this.changePage(1)}" 
+                ?disabled="${this.currentPage === this.totalPages}">
+                Next
+              </bbva-web-button-default>
+            </div>
+    
+            <div class="evolution-container ${this.selectedEvolutions ? 'show-evolution' : 'evolution-overlay'}">
+              ${this.selectedEvolutions && this.selectedEvolutions.length > 0 ? html`
+                <div class="evolution-content">
+                  <h1>Evolution Pokemon</h1>
+                  <div class="pokemon-text">
+                    ¡Pokémon evolutions are like watching your little friend go from cute to fearsome! Just a couple of levels, a flash of light, and boom! Now you have a powerful beast that can shoot bolts and crush rivals. It's the magic of levels and stats skyrocketing!
+                  </div>
+                  <div class="evolution-cards">
+                    ${this.selectedEvolutions.map(evolution => html`
+                      <div class="evolution-card">
                         <h3>${evolution.name}</h3>
                         <img src="${evolution.sprites.other.dream_world.front_default}" alt="${evolution.name}">
                         <p>Height: ${evolution.height}</p>
                         <p>Weight: ${evolution.weight}</p>
                         <p>Type: ${evolution.types.map(typeInfo => typeInfo.type.name).join(', ')}</p>
-                    </div>
-                `)}
+                      </div>
+                    `)}
+                  </div>
+                  <div class="button-container">
+                    <bbva-web-button-default @click="${this.goBackToList}">Previous</bbva-web-button-default> 
+                  </div>
+                </div>
+              ` : ''}
             </div>
-            <div class="button-container">
-                <bbva-web-button-default @click="${this.goBackToList}">Previous</bbva-web-button-default> 
-            </div>
+    
+            <bbva-help-modal
+              id="modal-error"
+              title-text="Evolution"
+              body-text="${this.modalMessage}"
+              illustration="success"
+              button-text="Close"
+              @bbva-help-modal-button-click="${this.close}">
+            </bbva-help-modal>
+    
+            <pokemon-dm></pokemon-dm>
           </div>
-        ` : ''}
-      </div>
-
-      <bbva-help-modal
-        id="modal-error"
-        header-show-left-icon
-        header-left-icon-aria-label="padlock"
-        title-text="Evolution"
-        body-text="${this.modalMessage}"
-        subtitle-text="Evolution Information"
-        secondary-link-text="Link"
-        illustration="success"
-        button-text="Close"
-        @bbva-help-modal-button-click="${this.closeModal}">
-      </bbva-help-modal>
-
-    `;
-  }
+        `;
+    }
+    
 }
 
